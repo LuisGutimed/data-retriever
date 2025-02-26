00c2acfd6f738168751956d2ed352478117dc366
Luis Gutierrez <lgutierrez.stgt@gmail.com>
2025-02-26 10:43:36 -0600

    fix: redis config lettuce

--- src/main/java/mx/com/system/dataretriever/config/RedisConfig.java
+++ src/main/java/mx/com/system/dataretriever/config/RedisConfig.java
@@ -1,31 +1,42 @@
 package mx.com.system.dataretriever.config;
 
 import java.time.Duration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 
 @Configuration
 @EnableCaching
 public class RedisConfig {
 
+  @Value("${spring.data.redis.host}")
+  private String redisHost;
+
+  @Value("${spring.data.redis.port}")
+  private int redisPort;
+
+  @Value("${spring.data.redis.password}")
+  private String redisPassword;
+
+  @Primary
   @Bean
   public RedisConnectionFactory redisConnectionFactory() {
-    return new LettuceConnectionFactory();
+    RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(redisHost, redisPort);
+    config.setPassword(redisPassword);
+    return new LettuceConnectionFactory(config);
   }
 
   @Bean
   RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
-
     RedisCacheConfiguration defaults = RedisCacheConfiguration.defaultCacheConfig()
         .entryTtl(Duration.ofMinutes(5));
-
-    return RedisCacheManager.builder(connectionFactory)
-        .cacheDefaults(defaults)
-        .build();
+    return RedisCacheManager.builder(connectionFactory).cacheDefaults(defaults).build();
   }
 }
